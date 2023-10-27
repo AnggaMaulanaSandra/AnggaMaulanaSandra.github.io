b6194d843f3ac9e80d9364e3b50f4672cd126271
balyazulfikar <balyazulfikar@gmail.com>
2023-10-27 11:03:49 +0700

    coba upload

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -2,23 +2,21 @@ export default function AboutMe() {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="./img/about-me.png" alt="About Me" />
+        <div className="circle-container">
+          <img src="./img/angga_rapi.jpg" alt="About Me" className="circle-image" />
+        </div>
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+            Fresh graduates of Ahmad Dahlan University with a GPA of 3.03 S1 Industrial Engineering, have an interest in Product Planning and Inventory Control, Talent Acquisition, Warehouse,
+            Purchasing, Supply Chain, Manufacturing, K3 Ergonomics, Quality Control, 2D & 3D Design, HR Training Development and Business Development.
           </p>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
-          </p>
+          <p className="hero--section-description">Muhammadiyah Student Association FTI Commissariat (2019 - 2020) Head of Tabligh Study and Islamic Studies Division</p>
+          <p className="hero--section-description">Muhammadiyah Student Association FTI Commissariat (2020-2021) Head of Arts, Culture and Sports Division</p>
+          <p className="hero--section-description">Real Work Lecture of Anak Bangsa Mengabdi UAD (2022) Head of Unit 3 KKN AB</p>
         </div>
       </div>
     </section>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,21 +3,18 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, I'm Angga Maulana Sandra</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
-            <br />
-            Developer
+            <span className="hero--section-title--color">Bachelor</span> <br />
+            Industrial Engineer
           </h1>
-          <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
-          </p>
+          <p className="hero--section-description">Fresh graduates of Ahmad Dahlan University with a GPA of 3.03 S1 Industrial Engineering.</p>
         </div>
-        <button className="btn btn-primary">Get In Touch</button>
       </div>
-      <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" />
+      <div className="hero--section--img ">
+        <div className="circle-container">
+          <img src="./img/angga-depan.jpg" alt="Hero Section" className="circle-image" />
+        </div>
       </div>
     </section>
   );
